f5851c5835ecc34b6b3573b3b8d3bf08e22c36e9
Avabin <andpio002@utp.edu.pl>
2017-05-14 13:23:11 +0200

    Deleted Lombok

--- src/main/scala/tk/totalapoc/LiquidManagerApplication.scala
+++ src/main/scala/tk/totalapoc/LiquidManagerApplication.scala
@@ -1,12 +1,10 @@
 package tk.totalapoc
 
-import lombok.extern.java.Log
 import org.springframework.boot.SpringApplication
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import tk.totalapoc.beans.TestClass
 import tk.totalapoc.config.LiquidManagerConfig
 
-@Log
 object LiquidManagerApplication extends App{
   SpringApplication run classOf[LiquidManagerConfig]
   var ctx = new AnnotationConfigApplicationContext(classOf[LiquidManagerConfig])
